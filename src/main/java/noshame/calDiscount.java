152d15c52d3b9f5968c0bfbf6fc5104886cc3b4c
Sierra Buchanan <Sierrabuchanan97@gmail.com>
2021-09-14 21:00:11 -0400

    second commit

--- src/main/java/noshame/calDiscount.java
+++ src/main/java/noshame/calDiscount.java
@@ -2,14 +2,12 @@ package noshame;
 
 public class calDiscount {
     public static double calDiscount(double price, double dis) {
-       double product = 15.0;
-       double discount = 5.0;
-       double finishedp = product * discount;
-       double finp = product - finishedp;
+       double finishedp = price * dis;
+       double finp = price - finishedp;
        return finp;
     }
     public static void main(String[] args) {
-        System.out.println(calDiscount(15.0, 0.80));
+        System.out.println(calDiscount(100.0, 0.20));
     }
 }
 
